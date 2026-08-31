3cb8372db126c60f370d09bd5415e62ea84bc964
arako <79412889+ahra1221@users.noreply.github.com>
2026-08-31 22:07:36 +0900

    [level 3] Title: 단속카메라, Time: 6.69 ms, Memory: 58.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] routes) {
+        int answer = 1;
+        Arrays.sort(routes, (a,b) -> a[1] - b[1]);
+        int camera = routes[0][1];
+        for(int i=1;i<routes.length;i++) {
+            if(routes[i][0] > camera || routes[i][1] < camera) {
+                answer ++;
+                camera = routes[i][1];
+            }
+        }
+        return answer;
+    }
+}
