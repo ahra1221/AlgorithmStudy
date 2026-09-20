32507cf65e07359016f2881f42e4c2517833a3f2
arako <79412889+ahra1221@users.noreply.github.com>
2026-09-20 19:09:47 +0900

    [level 2] Title: 리코쳇 로봇, Time: 3.74 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/README.md
+++ 프로그래머스/2/169199. 리코쳇 로봇/README.md
@@ -1,10 +1,10 @@
 # [level 2] 리코쳇 로봇 - 169199 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/169199?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/169199) 
 
 ### 성능 요약
 
-메모리: 77.8 MB, 시간: 1.90 ms
+메모리: 77 MB, 시간: 3.74 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 18일 10:30:22
+2026년 09월 20일 19:09:44
 
 ### 문제 설명
 
--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -1,56 +1,67 @@
 import java.util.*;
 
 class Solution {
+    
+    class Point {
+        int x;
+        int y;
+        int cnt;
+        
+        Point(int x, int y, int cnt) {
+            this.x = x;
+            this.y = y;
+            this.cnt = cnt;
+        }
+    }
+    
     public int solution(String[] board) {
-        int answer = -1;
-        int r = board.length, c = board[0].length();
-        int[] start = new int[2];
-        char[][] map = new char[r][c];
+        int r = board.length;
+        int c = board[0].length();
+        
+        Point start = new Point(0,0,0);
+        Point end = new Point(0,0,0);
+        Character[][] map = new Character[r][c];
+        
         for(int i=0;i<r;i++) {
-            char[] ch = board[i].toCharArray();
             for(int j=0;j<c;j++) {
-                char tmp = ch[j];
-                if (tmp == 'R') {
-                    start[0] = i;
-                    start[1] = j;
-                }
-                map[i][j] = tmp;
+                char ch = board[i].charAt(j);
+                if(ch == 'R') start = new Point(i,j,0);
+                if(ch == 'G') end = new Point(i,j,0);
+                map[i][j] = ch;
             }
         }
         
-        Queue<int[]> queue = new LinkedList<>();
-        boolean[][] visited = new boolean[r][c];
-        queue.offer(new int[]{start[0], start[1],0});
-        visited[start[0]][start[1]] = true;
-        
-        int[] dx = {-1,1,0,0};
+        int[] dx = {1,-1,0,0};
         int[] dy = {0,0,1,-1};
         
+        Queue<Point> queue = new ArrayDeque<>();
+        boolean[][] visited = new boolean[r][c];
+        queue.offer(start);
+        visited[start.x][start.y] = true;
+        
         while(!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            int x = cur[0], y = cur[1], cnt = cur[2];
-            if(map[x][y] == 'G') {
-                answer = cnt;
-                break;
+            Point cur = queue.poll();
+            if(cur.x == end.x && cur.y == end.y) {
+                return cur.cnt;
             }
-            for(int d=0;d<4;d++) {
-                int cx = x, cy = y;
+            
+            for(int i=0;i<4;i++) {
+                int nx = cur.x, ny = cur.y, nc = cur.cnt;
                 while(true) {
-                    int nx = cx + dx[d];
-                    int ny = cy + dy[d];
-                    if(nx < 0 || nx >= r || ny < 0 || ny >= c) break;
-                    if(map[nx][ny] == 'D') break;
-                    cx = nx;
-                    cy = ny;
+                    int tmpx = nx + dx[i];
+                    int tmpy = ny + dy[i];
+                    if(tmpx < 0 || tmpx >= r || tmpy < 0 || tmpy >= c) break;
+                    if(map[tmpx][tmpy] == 'D') break;
+                    nx = tmpx;
+                    ny = tmpy;
                 }
                 
-                if(!visited[cx][cy]) {
-                    queue.offer(new int[]{cx,cy,cnt+1});
-                    visited[cx][cy] = true;
-                }
+                if(visited[nx][ny]) continue;
+                queue.offer(new Point(nx,ny,nc+1));
+                visited[nx][ny] = true;
             }
         }
         
-        return answer;
+        return -1;
     }
-}   
+}
